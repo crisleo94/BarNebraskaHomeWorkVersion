33f4974ac83b290d281e4f30097016176397d54d
creinosolt <creinoso@lean-tech.io>
2020-01-18 16:16:52 -0500

    form ready to make the post request, still something to fix wit the http response from the post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +21,9 @@ material.AutoInit();
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import * as material from '../../../assets/js/materialize.min.js';
-
+import { Contact } from '../../models/contact'
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -18,11 +20,21 @@ export class HomeComponent implements OnInit {
     hoverEnabled: false
   };
 
-  constructor() { }
+  modalOptions = {
+    onCloseEnd: this.onSubmit()
+  }
+
+  contact = new Contact('', '');
+  API_URI = 'https://api-bar-nebraska.herokuapp.com/savecontact';
+
+  constructor(private http: HttpClient) {
+    console.log(this.contact);
+  }
 
   ngOnInit() {
     this.carousel();
     this.fabButton();
+    this.modal();
   }
 
   carousel() {
@@ -33,6 +45,24 @@ export class HomeComponent implements OnInit {
   fabButton() {
     const element = document.querySelectorAll('.fixed-action-btn');
     const instance = material.FloatingActionButton.init(element, this.fabOptions);
+  }
+
+  modal() {
+    const element = document.querySelectorAll('.modal');
+    const instance = material.Modal.init(element);
+  }
+
+  onSubmit() {
+    return this.http.post(this.API_URI, this.contact).toPromise()
+      .then(req => {
+        console.log(req);
+        this.contact.email = '';
+        this.contact.message = '';
+      }).catch(err => {
+        if(err) {
+          console.error(err);
+        }
+      })
   }
 
 }
